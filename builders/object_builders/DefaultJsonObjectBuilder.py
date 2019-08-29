59efa659d4e858730ea21781a4d5c59eade60624
vengefire <vengefire@gmail.com>
2019-08-29 16:36:53 +0800

    Fixing up bad json is a pain

--- builders/object_builders/DefaultJsonObjectBuilder.py
+++ builders/object_builders/DefaultJsonObjectBuilder.py
@@ -1,5 +1,5 @@
 from os import listdir
-from os.path import join
+from os.path import join, isdir
 from os.path import splitext
 
 from builders.object_builders.object_builder import ObjectBuilder
@@ -11,16 +11,23 @@ class DefaultJsonObjectBuilder(ObjectBuilder):
         super().__init__(object_type)
 
     def build_objects(self):
+        if not isdir(self.manifest_entry.full_path):
+            self._logger.warn(f'The specified manifest path [{self.manifest_entry.full_path}] does not exist.')
+            return []
+
         files = [join(self.manifest_entry.full_path, filename) for filename in listdir(self.manifest_entry.full_path) if splitext(filename)[1] == '.json']
+        objects = []
         for file in files:
             self._logger.debug(f'Processing [{file}]...')
             json = load_json_from_file(file)
             description = json['Description'] if 'Description' in json else None
             if description is not None:
-                id = description['Id']
+                identifier = description['Id']
                 name = description['Name']
                 ui_name = description['UIName'] if 'UIName' in description else None
                 icon = description['Icon'] if 'Icon' in description else None
             else:
-                id = json['Id'] if 'Id' in json else json['ID'] if 'ID' in json else None
-            self._logger.debug(f'Found Id [{id}]')
+                identifier = json['Id'] if 'Id' in json else json['ID'] if 'ID' in json else None
+            self._logger.debug(f'Found Id [{identifier}]')
+
+        return objects
--- util/json.py
+++ util/json.py
@@ -7,7 +7,26 @@ from util.string import remove_comments
 
 def remove_extraneous_commas(string):
     string = re.sub(r",\s*}", "}", string)
-    string = re.sub(r",\s*]", "]", string)
+    return re.sub(r",\s*]", "]", string)
+
+
+def unescape_quotes(string):
+    return string.replace(r"\'", r"'")
+
+
+def delimit_consecutive_arrays(string):
+    return re.sub(r"]\s*\[", '],[', string)
+
+
+def delimit_consecutive_properties(string):
+    # un-delimited consecutive objects
+    string = re.sub(r"}\s*{", '},{', string)
+    # un-delimited consecutive arrays
+    string = re.sub(r"]\s*\"", '],"', string)
+    # un-delimited consecutive quoted properties
+    string = re.sub(r"\"\s*\"", '","', string)
+    # un-delimited consecutive non-quoted properties
+    string = re.sub(r"([^{},[:\s]$\s*)\"", "\g<1>,\"", string, flags=re.MULTILINE)
     return string
 
 
@@ -15,5 +34,8 @@ def load_json_from_file(path):
     with tokenize.open(path) as file:
         string = file.read()
         string = remove_comments(string)
+        string = unescape_quotes(string)
         string = remove_extraneous_commas(string)
+        string = delimit_consecutive_arrays(string)
+        string = delimit_consecutive_properties(string)
         return json.loads(string)
